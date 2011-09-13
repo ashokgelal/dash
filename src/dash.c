b04337a1f430f204bf288570ae73ef539d9b151a
Ashok Gelal <ashokgelal@gmail.com>
2011-09-13 16:07:59 -0700

    Initial commit

---
+++ src/dash.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : dash.c
+ Author      : Ashok Gelal
+ Version     :
+ Copyright   : All Wrongs Reserved
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
