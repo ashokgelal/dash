3595948eac44b3b49dbc0d9665755b60c5dcbe07
Ashok Gelal <ashokgelal@gmail.com>
2011-09-15 00:22:58 -0700

    Reads DASH_PROMT environment variable for prompt.
    Also, building up my git-commit portfolio :)

--- src/dash.c
+++ src/dash.c
@@ -10,8 +10,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
 int main(void) {
+	char *s;
+	s = getenv("DASH_PROMPT");
+	if(s==NULL || *s =='\0')
+		s = ":) ";
 	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
